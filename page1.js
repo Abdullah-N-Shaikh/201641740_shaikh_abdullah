d5d1a787ec0cc2bed23d3d4be74783f43e985e9f
Abdullah-N-Shaikh <69971937+Abdullah-N-Shaikh@users.noreply.github.com>
2022-02-19 19:35:13 +0300

    Add files via upload

---
+++ page1.js
@@ -0,0 +1,4 @@
+function show() {
+   alert("You are in page 1");
+   console.log("You are in page 1");
+}
---
+++ page3.js
@@ -0,0 +1,4 @@
+ function show() {
+   alert("You are in page 3");
+   console.log("You are in page 3");
+}
